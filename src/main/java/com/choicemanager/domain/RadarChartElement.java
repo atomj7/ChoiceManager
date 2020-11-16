fbad71f3c4c4e00a1b2a4312e3c742edd8196c1c
Mark <markisby@gmail.com>
2020-11-16 18:39:31 +0300

    Radar chart demo

---
+++ src/main/java/com/choicemanager/domain/RadarChartElement.java
@@ -0,0 +1,14 @@
+package com.choicemanager.domain;
+
+import lombok.Data;
+
+@Data
+public class RadarChartElement {
+    String categoryName;
+    Double averageValue;
+
+    public RadarChartElement(String categoryName, Double averageValue) {
+        this.categoryName = categoryName;
+        this.averageValue = averageValue;
+    }
+}
